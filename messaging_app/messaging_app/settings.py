0752e650ca57f0decb114824084fb38fbf5daefa
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-16 05:05:54 +0100

    add chat

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
+
+AUTH_USER_MODEL = 'chats.User'
 
 TEMPLATES = [
     {
