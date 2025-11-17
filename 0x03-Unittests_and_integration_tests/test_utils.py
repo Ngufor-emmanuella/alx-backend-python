6d98bce2ca34063721277fe16977198971d4f488
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-17 10:55:19 +0100

    Add utils.py file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,23 @@
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch
+from utils import memoize
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-        self.assertEqual(cm.exception.args[0], path[-1])
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            first_call = test_obj.a_property
+            second_call = test_obj.a_property
+
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_method.assert_called_once()
