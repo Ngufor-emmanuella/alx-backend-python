c295f3a5da6616e5ee86693af5e6d92c67471000
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-17 11:11:49 +0100

    Add test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,22 +1,14 @@
 import unittest
-from unittest.mock import patch
+from parameterized import parameterized
 from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
 
-    @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
-        mock_get_json.return_value = [
-            {"name": "repo1"},
-            {"name": "repo2"}
-        ]
-
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=unittest.mock.PropertyMock) as mock_url:
-            mock_url.return_value = "http://fake-url.com"
-
-            client = GithubOrgClient("any_org")
-            repos = client.public_repos
-
-            self.assertEqual(repos, ["repo1", "repo2"])
-            mock_url.assert_called_once()
-            mock_get_json.assert_called_once_with("http://fake-url.com")
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        client = GithubOrgClient("any_org")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
