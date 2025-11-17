3283b3fe463c700e94a4b3fc0eec86153233e7eb
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-17 11:09:29 +0100

    Add test.py file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,8 +3,20 @@ from unittest.mock import patch
 from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
-    def test_public_repos_url(self):
-        with patch.object(GithubOrgClient, "org", new_callable=unittest.mock.PropertyMock) as mock_org:
-            mock_org.return_value = {"repos_url": "http://fake-url.com"}
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"}
+        ]
+
+        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=unittest.mock.PropertyMock) as mock_url:
+            mock_url.return_value = "http://fake-url.com"
+
             client = GithubOrgClient("any_org")
-            self.assertEqual(client._public_repos_url, "http://fake-url.com")
+            repos = client.public_repos
+
+            self.assertEqual(repos, ["repo1", "repo2"])
+            mock_url.assert_called_once()
+            mock_get_json.assert_called_once_with("http://fake-url.com")
