9cdbc191f1e5419fc8ae3b7e62147e1778b8445e
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-23 10:10:09 +0100

    Add test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,23 +1,16 @@
 import unittest
-from unittest.mock import patch
-from utils import memoize
+from parameterized import parameterized
+from utils import access_nested_map
 
-class TestMemoize(unittest.TestCase):
-    def test_memoize(self):
-        class TestClass:
-            def a_method(self):
-                return 42
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-            @memoize
-            def a_property(self):
-                return self.a_method()
 
-        test_obj = TestClass()
-
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-            first_call = test_obj.a_property
-            second_call = test_obj.a_property
-
-            self.assertEqual(first_call, 42)
-            self.assertEqual(second_call, 42)
-            mock_method.assert_called_once()
+if __name__ == "__main__":
+    unittest.main()
