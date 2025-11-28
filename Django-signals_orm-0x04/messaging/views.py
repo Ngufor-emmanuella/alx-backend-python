26399115b45853384acbc4de3e4ec1e0a25c7ffe
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-28 16:40:23 +0100

    Add cache

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Message
+from django.views.decorators.cache import cache_page
+
 
 @login_required
 def inbox_view(request):
@@ -12,3 +14,17 @@ def inbox_view(request):
     )
     
     return render(request, 'messaging/inbox.html', {'unread_messages': unread_messages})
+
+
+
+@login_required
+@cache_page(60)  # Cache this view for 60 seconds
+def conversation_view(request):
+    # Fetch messages for the logged-in user
+    messages = (
+        Message.objects.select_related('sender')
+        .filter(receiver=request.user)
+        .order_by('timestamp')
+    )
+    
+    return render(request, 'messaging/conversation.html', {'messages': messages})
